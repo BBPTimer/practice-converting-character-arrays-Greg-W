9e98f5cb20403d7513735a4eabd4d1d9a5ec0982
gweseloh <greg.weseloh@gmail.com>
2025-03-13 09:42:48 -0500

    Reversed inspirational messages

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+let message1 = "!yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+console.log(message1.split("").reverse().join(""));
 
+let message2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+console.log(message2.split("").reverse().join(""));
+
+let message3 = "!elpoep rehto morf tnereﬃd on era uoy ,elbuort evah uoy fI. lanoisseforp a ekil leef ot evah t'nod uoY";
+console.log(message3.split("").reverse().join(""));
+
+let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+console.log(message4.split("").reverse().join(""));
+
+let message5 = "I'm good enough, I'm smart enough, and doggone it, people like me.";
+console.log(message5.split("").reverse().join(""));
+
+let message6 = "I am lovable and capable.";
+console.log(message6.split("").reverse().join(""));
